f419a1cf1e20d02e8da3355a731167ed24f543f5
abdullah keleş <abdullah.keles@hotmail.com.tr>
2020-02-19 06:24:02 +0300

    diziler bitti

--- js-temelleri/diziler.js
+++ js-temelleri/diziler.js
@@ -14,9 +14,12 @@ brands.push("renault");
 console.log(brands);
 
 //splice dizinin herhangi bir elemanını siler
+// birinci parametre ile silme işlemine balanacak dizi elamanı işaretlernir.
+// ikinci parametre ile kaç elamanın silineceği belirtilir.
 brands.splice(brands.indexOf("renault"),1);
 brands.splice(brands.indexOf("toyat"),1);
 console.log(brands);
+
 function fnUpperCase(val){
     return val.toUpperCase();
 }
@@ -49,18 +52,65 @@ console.log(brands.includes("tofaş"));
 // reduce() fonksiyonu, data dizisinin her bir elemanı kadar reducer fonksiyonunu çağırır. 
 // Bu işlemin sonucunu kümülatif olarak hesaplar.
 // Yapılan her işlemin sonucu bir sonraki fonksiyon çağırımına ilk parametre olarak aktarılır.
-let numbers=[1,2,3,4,5];
+let numbers=[23,1,2,44,3,36,4,5];
 var total=numbers.reduce((a,b)=> a+b);
 console.log(total);
 var multiplication=numbers.reduce((a,b)=> a*b);
 console.log(multiplication);
+console.log(numbers.sort((a,b)=> a-b ));
+
 
 // some() fonksiyonu, herhangi bir dizide yer alan elemanlardan 
 // en az bir tanesinin sağlanan koşulu geçip geçmediğini kontrol eder.
-
-var isThreeBigger=numbers.some((el)=> el>3);
-console.log(isThreeBigger);
+var isThreeBigger=numbers.some((el)=> el===1);
+console.log("is sum : "+isThreeBigger);
 
 //concat() metodu eklendigi dizi ile parametre olarak aldığı dizi(leri) birleştirerek yeni bir dizi döndürür.
 var margeArray=numbers.concat(brands);
 console.log(margeArray);
+
+
+//Eğer compareFunctionsağlanmaz, 
+//olmayan tüm undefineddizi elemanları UTF-16 kod birimleri sırayla dizeleri ve karşılaştırma 
+//şeritlerine dönüştürerek sıralanır. Örneğin, 
+//"muz" "kiraz" dan önce gelir. 
+//Sayısal bir sıralamada 9, 80'den önce gelir, 
+//ancak sayılar dizelere dönüştürüldüğünden, "80", 
+//Unicode sırasıyla "9" dan önce gelir. Tüm  undefined öğeler dizinin sonuna kadar sıralanır.
+
+var numbers2 = [4, 2, 5, 1, 3];
+numbers2.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers2);
+
+// [1, 2, 3, 4, 5]
+
+var items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic', value: 13 },
+    { name: 'Zeros', value: 37 }
+  ];
+  
+  // sort by value
+  items.sort(function (a, b) {
+    return a.value - b.value;
+  });
+  
+  // sort by name
+  items.sort(function(a, b) {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
+  });
